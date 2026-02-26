e257b9a3c094a574fcaa917b495734a3d74e13b1
grpchatdstryr <pnate027@gmail.com>
2026-02-26 13:21:17 -0500

    Add Search Bar & Migrate to nathanpatrick.net

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -180,7 +180,41 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
 
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'KBYGMT9VC2',
 
+      // Public API key: it is safe to commit it
+      apiKey: '1750c14047829bdd25705d5e5b1725e1',
+
+      indexName: 'nathan_patrick_pages',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      //replaceSearchResultPathname: {
+      //  from: '/docs/', // or as RegExp: /\/docs\//
+      //  to: '/',
+      //},
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      // Optional: whether you want to use the new Ask AI feature (undefined by default)
+      //askAi: 'YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID',
+
+      //... other Algolia params
+    },
 
 
 
